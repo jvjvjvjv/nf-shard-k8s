186150de82dacdeef867b5bf2f94f2739b8a2fe7
root <jvjvjvjv>
2026-01-06 00:47:32 +0000

    Helm bug fixes

--- deploy_helm-shared-pg-server.sh
+++ deploy_helm-shared-pg-server.sh
@@ -1,15 +1,16 @@
 # Generate a standard-looking uuid
 uuid=$(cat /proc/sys/kernel/random/uuid)
-helm install user1-nfshard ./helm \
-        --set user.name=user1 \
-        --set user.username=user1 \
-        --set user.password=user1pass \
+userid='user2'
+helm install "$userid"-nfshard ./helm_shared-pg-server --namespace nf-shard --create-namespace \
+        --set user.name="$userid" \
+        --set user.username="$userid" \
+        --set user.password="$userid"pass \
         --set app.secretKey=$(openssl rand -hex 32) \
         --set app.defaultAccessToken="$uuid" \
 	--set app.image.repository=registry.rc.umass.edu:5050/harmony/infrastructure/user-resources/nf-shard \
         --set app.image.tag=base \
 	--set app.image.pullPolicy=Always \
-        --set app.service.nodePort=30000 \
+        --set app.service.nodePort=30001 \
         --set postgres.auth.user=postgres \
-        --set postgres.auth.password=password \
+        --set postgres.auth.password=your-secret-password \
         --set postgres.auth.host=postgres-0.postgres.postgres-shared.svc.cluster.local
