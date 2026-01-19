94f6e6ffae0aa408bcf71f4a2d6e9f38d8b269bf
root <jvjvjvjv>
2026-01-19 21:17:16 +0000

    Remove nextflow refs from shard helm chart

--- deploy_helm-shared-pg-server.sh
+++ deploy_helm-shared-pg-server.sh
@@ -1,6 +1,6 @@
 # Generate a standard-looking uuid
 uuid=$(cat /proc/sys/kernel/random/uuid)
-userid='user2'
+userid='user1'
 helm install "$userid"-nfshard ./helm_shared-pg-server --namespace nf-shard --create-namespace \
         --set user.name="$userid" \
         --set user.username="$userid" \
@@ -12,5 +12,6 @@ helm install "$userid"-nfshard ./helm_shared-pg-server --namespace nf-shard --cr
 	--set app.image.pullPolicy=Always \
         --set app.service.nodePort=30001 \
         --set postgres.auth.user=postgres \
-        --set postgres.auth.password=your-secret-password \
+        --set postgres.auth.password=password \
         --set postgres.auth.host=postgres-0.postgres.postgres-shared.svc.cluster.local
+#        --set postgres.auth.host=postgres-0.postgres.postgres-shared.svc.cluster.local
